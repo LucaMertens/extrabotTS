5db75ed0dd0fa34dddf2dfd9ca75471b1f0e3e9f
Marc Leibold <29206905+marcleibold@users.noreply.github.com>
2020-03-21 22:30:49 +0100

    refactored Event Handling

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ require("dotenv").config();
 import { Client, Collection } from "discord.js";
 import { readdirSync } from "fs";
 import { Command } from "./commands/Command";
+import { Event } from "./events/Event";
 import { ConfigHandler } from "./config/ConfigHandler";
 
 const client: Client = new Client();
 let commands: Collection<string, Command>;
+let events: Collection<string, Event>;
 client.login(process.env.TOKEN);
 
 const loadCommands = async (): Promise<void> => {
@@ -18,27 +20,38 @@ const loadCommands = async (): Promise<void> => {
   });
 };
 
-client.on("ready", async () => {
-  const loadCommandsPromise = loadCommands();
-  console.log("Extrabot is ready for some dank memes.");
-  client.user!.setActivity(ConfigHandler.get("defaultActivity"));
-  await loadCommandsPromise;
-});
+const loadEvents = async (): Promise<void> => {
+  event = new Collection();
+  readdirSync("./src/events").forEach(file => {
+    const event: Event = require("./events/" + file);
+    client.on(file, event);
+  });
+};
 
-client.on("disconnect", () => console.warn("Disconnected!"));
+loadCommands();
+loadEvents();
 
-client.on("message", async message => {
-  if (message.author!.bot || message.content == null) return;
-  const prefix = ConfigHandler.get("prefix");
-  // checkForPizza();
-  if (!message.content.toLowerCase().startsWith(prefix)) return;
-  // Split the message at every whitespace character.
-  const args = message.content.substring(prefix.length).split(/ +/g);
+// client.on("ready", async () => {
+//   const loadCommandsPromise = loadCommands();
+//   console.log("Extrabot is ready for some dank memes.");
+//   client.user!.setActivity(ConfigHandler.get("defaultActivity"));
+//   await loadCommandsPromise;
+// });
 
-  const command = args.shift()!.toLowerCase();
-  if (commands.has(command)) {
-    commands.get(command)!.execute(message);
-  } else {
-    message.channel.send("WATI WAT");
-  }
-});
+// client.on("disconnect", () => console.warn("Disconnected!"));
+
+// client.on("message", async message => {
+//   if (message.author!.bot || message.content == null) return;
+//   const prefix = ConfigHandler.get("prefix");
+//   // checkForPizza();
+//   if (!message.content.toLowerCase().startsWith(prefix)) return;
+//   // Split the message at every whitespace character.
+//   const args = message.content.substring(prefix.length).split(/ +/g);
+
+//   const command = args.shift()!.toLowerCase();
+//   if (commands.has(command)) {
+//     commands.get(command)!.execute(message);
+//   } else {
+//     message.channel.send("WATI WAT");
+//   }
+// });
