4aa178c545676b7a0cf5cc5ac719b15da701cc6c
Marc Leibold <29206905+marcleibold@users.noreply.github.com>
2020-04-15 15:10:00 +0200

    Moved Command and Event loading functions to utils.ts
    Co-authored-by: Luca Mertens <luca.mertens00@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 // Start of bot
 import { Client, Collection } from "discord.js";
 import { readdirSync } from "fs";
-import { getCommands } from "./getCommands";
+import { getCommands, loadEvents } from "./utils";
 import { DropboxHandler } from "./themes/DropboxHandler";
 import { ThemeHandlerInterface } from "./themes/ThemeHandlerInterface";
 import { Command } from "./types/Command";
@@ -11,14 +11,6 @@ import { Command } from "./types/Command";
 export const themeHandler: ThemeHandlerInterface = new DropboxHandler();
 export let commands: Collection<string, Command>;
 export const client: Client = new Client();
-
-const loadEvents = async (): Promise<void> => {
-  readdirSync(`${__dirname}/events`).forEach(fileName => {
-    if (!fileName.endsWith(".js")) return;
-    // This makes sure that every "client.on" - eventhandler is included.
-    import("./events/" + fileName);
-  });
-};
 
 const init = async () => {
   commands = await getCommands();
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,30 @@
+import { Collection } from "discord.js";
+import { readdirSync } from "fs";
+import { Command, isCommand } from "./types/Command";
+
+export const getCommands = async (): Promise<Collection<string, Command>> => {
+  const commands = new Collection<string, Command>();
+  readdirSync(`${__dirname}/commands`).forEach(async fileName => {
+    // Ignore non-js files.
+    if (!fileName.endsWith(".js")) return;
+    const command = await import("./commands/" + fileName);
+    if (!isCommand(command)) {
+      console.log(`Skipping file ${fileName}, because it's not a command.`);
+      return;
+    }
+    // Remove the ".js" suffix.
+    const commandName = fileName.slice(0, -3);
+    commands.set(commandName, command);
+  });
+  return commands;
+};
+
+export const loadEvents = async (): Promise<void> => {
+  readdirSync(`${__dirname}/events`).forEach(fileName => {
+    if (!fileName.endsWith(".js")) return;
+    // This makes sure that every "client.on" - eventhandler is included.
+    import("./events/" + fileName);
+  });
+};
+
 export const pickRandom = (array: any[]) => array[Math.floor(Math.random() * array.length)];
