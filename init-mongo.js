a006ec5d1b8054b621039e613371548bb296d8a6
Luca Mertens <luca.mertens00@gmail.com>
2020-12-28 22:44:05 +0100

    Docker Mongo-DB Setup - 1 of many

---
+++ init-mongo.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+db.createUser({
+  user: process.env.MONGO_INITDB_DATABASE,
+  pwd: process.env.MONGO_INITDB_ROOT_USERNAME,
+  roles: [
+    {
+      role: "readWrite",
+      db: process.env.MONGO_INITDB_ROOT_PASSWORD
+    }
+  ]
+});
