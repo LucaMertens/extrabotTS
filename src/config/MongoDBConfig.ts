c81c383a1c2944121d7f3209a905d2be1d2de4b8
Marc Leibold <leibold485@gmail.com>
2020-12-29 21:58:48 +0100

    base implementation of MongoDBConfig

--- src/config/MongoDBConfig.ts
+++ src/config/MongoDBConfig.ts
@@ -2,36 +2,61 @@
 import { ConfigInterface, GuildConfig, GlobalConfig, UserConfig } from "./ConfigInterface";
 import { config, DynamoDB } from "aws-sdk";
 import { Guild, User } from "discord.js";
+import { MongoClient, Db } from "mongodb";
 
 export class MongoDBConfig implements ConfigInterface {
-  constructor() {}
-  getGlobalEntry<K extends keyof GlobalConfig>(property: K): Promise<GlobalConfig[K]> {
+  url = "mongodb://mongo-db:27017/";
+  dbClient: MongoClient;
+  database: Promise<Db>;
+
+  constructor() {
+    this.dbClient = new MongoClient(this.url);
+    this.database = this.getDatabasePromise();
+  }
+
+  async getDatabasePromise(): Promise<Db> {
+    await this.dbClient.connect();
+    return this.dbClient.db("database");
+  }
+
+  async getGlobalEntry<K extends keyof GlobalConfig>(property: K): Promise<GlobalConfig[K]> {
     throw new Error("Method not implemented.");
   }
-  getGuildEntry<K extends keyof GuildConfig>(guild: Guild, property: K): Promise<GuildConfig[K]> {
+  async getGuildEntry<K extends keyof GuildConfig>(
+    guild: Guild,
+    property: K
+  ): Promise<GuildConfig[K]> {
+    const database = await this.database;
+    const collection = database.collection("guilds");
+  }
+  async getUserEntry<K extends keyof UserConfig>(user: User, property: K): Promise<UserConfig[K]> {
+    const database = await this.database;
     throw new Error("Method not implemented.");
   }
-  getUserEntry<K extends keyof UserConfig>(user: User, property: K): Promise<UserConfig[K]> {
+  async setGlobalEntry<K extends keyof GlobalConfig>(
+    key: K,
+    value: GlobalConfig[K]
+  ): Promise<boolean> {
     throw new Error("Method not implemented.");
   }
-  setGlobalEntry<K extends keyof GlobalConfig>(key: K, value: GlobalConfig[K]): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
-  setGuildEntry<K extends keyof GuildConfig>(
+  async setGuildEntry<K extends keyof GuildConfig>(
     guild: Guild,
     key: K,
     value: GuildConfig[K]
   ): Promise<boolean> {
+    const database = await this.database;
     throw new Error("Method not implemented.");
   }
-  setUserEntry<K extends keyof UserConfig>(
+  async setUserEntry<K extends keyof UserConfig>(
     user: User,
     key: K,
     value: UserConfig[K]
   ): Promise<boolean> {
+    const database = await this.database;
     throw new Error("Method not implemented.");
   }
-  isAdmin(guild: Guild, userId: string): Promise<boolean> {
+  async isAdmin(guild: Guild, userId: string): Promise<boolean> {
+    const database = await this.database;
     throw new Error("Method not implemented.");
   }
 }
